08bf48ba718770a97b0c03ac882db04728bc2d88
giank123 <giank.07.pm@gmail.com>
2022-04-12 13:52:10 -0500

    oye abre tus ojos :v

---
+++ src/test/java/awsGateway/springbootawsGateway/SpringbootAwsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package awsGateway.springbootawsGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
